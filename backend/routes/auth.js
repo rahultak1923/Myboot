784580bad73b6dda08f6c11a15f66e6527cbbd65
Rahul Tak <rahultak955@gmail.com>
2024-07-13 20:59:38 +0530

    thunderclient request send apply

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
 
-router.get('/', (req,res)=>{
+router.post('/', (req,res)=>{
   console.log(req.body);
-  res.send("hello");
+  const user = User(req.body);
+  user.save()
+  res.send(req.body);
+
 })
 
 module.exports = router
